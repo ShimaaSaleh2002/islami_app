daaefe9cbfe1b584c8c64c5794ea2abee32eb36c
shimaaa_saleh <123778598+ShimaaSaleh2002@users.noreply.github.com>
2024-07-10 16:25:26 +0300

    app initialization

---
+++ lib/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+   static const routeName = 'home';
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('islami'),
+      ),
+    );
+  }
+}
